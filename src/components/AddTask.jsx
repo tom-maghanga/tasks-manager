319dd6011aabe364e695221de61d5bd0e422c666
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2024-05-02 04:26:16 +0300

    Final project

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,24 +1,12 @@
-import React from 'react'
-
-function AddTask({taskList, setTaskList}) {
-    const handleSumbit = (e) =>{
-        e.preventDefault();
-
-        const date = new Date();
-        const newTask = {id : date.getTime(), name : e.target.task.value, time : `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`};
-        setTaskList([...taskList, newTask]);
-        e.target.task.value = "";
-    }
-
-
+const AddTask = ({handleSubmit, editid, task, setTask}) => {
   return (
-    <section className='addTask'>
-        <form onSubmit={handleSumbit}>
-            <input type='text' name='task' autoComplete='off' placeholder='add task' maxLength={25}/>
-            <button type='submit'> Add</button>
+      <section className='addTask'>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="task" value={task} autoComplete="off" placeholder="add task" maxLength="25" onChange={(e) => setTask(e.target.value)}/>
+          <button type="submit">{ editid ? "Update" : "Add"}</button>
         </form>
-    </section>
-  )
+      </section>
+  );
 }
 
-export default AddTask
+export default AddTask;
--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -1,31 +1,27 @@
-import React from 'react'
-
-function ShowTask({taskList, setTaskList}) {
-   
-  return (
-    <section className='showTask'>
-        <div className='head'>
-            <div>
-                <span className='title'></span>
-                <span className='count'></span>
-            </div>
-            <button className='clearAll'>Clear All</button>
-        </div>
-        <ul>
-            {taskList.map((task) =>(
-                <li>
-                <p>
-                     <span className='name'>{task.name}</span>
-                     <span className='time'>{task.time}</span>
-                 </p>
-                 <i className='bi bi-pencil-square'></i>
-                 <i className='bi bi-trash'></i>
-                </li>
-            ))}
-           
-        </ul>
-    </section>
-  )
+const ShowTask = ({tasklist, setTasklist, handleEdit, handleDelete}) => {
+    return (
+        <section className='showTask'>
+            <p className="head">
+                <span>
+                    <span className="title">Todo</span>
+                    <span className="count">{tasklist.length}</span>
+                </span>
+                <span className="clearAll" onClick={() => setTasklist([])}>Clear All</span>
+            </p>
+            <ul>
+                {tasklist.map((task) => (
+                    <li key={task.id}>
+                        <p>
+                            <span className="name">{task.name}</span>
+                            <span className="time">{task.time}</span>
+                        </p>                
+                        <i className="bi bi-pencil-square" onClick={() => handleEdit(task.id)}></i>
+                        <i className="bi bi-trash" onClick={() => handleDelete(task.id)}></i>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
 }
 
-export default ShowTask
+export default ShowTask;
